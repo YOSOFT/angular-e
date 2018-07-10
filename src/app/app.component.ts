bc6eeaf0e63963ff2ef91751f4da1ae7e56ac4db
Julio Cesar Ramirez Ospina <julio.ramirez@icbf.gov.co>
2018-07-10 13:19:25 -0500

    inicio de aplicacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: ``,
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'Visualizacion de datos';
+  dias = ['Lunes-----', 'Martes----', 'Miercoles', 'Jueves----', 'Viernes---', 'Sabado---', 'Domingo-'];
+  constructor() { }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.title = "Cambio la propiedad";
+      this.dias.push('nuevo valor');
+    }, 5000);
+  }
 }
